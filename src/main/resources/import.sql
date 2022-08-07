dbc4ed04fb881b55c50b933535027f40ad9f5e00
VinicioSichiqui <vinicio1998sichiqui@outlook.com>
2022-08-07 16:33:08 -0500

    actualizo import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,33 @@
 
 
 INSERT INTO marca (nombre_marca) VALUES  ('Rufles');
+INSERT INTO marca (nombre_marca) VALUES  ('Doritos');
+INSERT INTO marca (nombre_marca) VALUES  ('Coca cola');
 
 INSERT INTO presentacion (nombre_presentacion) VALUES  ('35gr');
+INSERT INTO presentacion (nombre_presentacion) VALUES  ('50gr');
+INSERT INTO presentacion (nombre_presentacion) VALUES  ('170ml');
+
 
 INSERT INTO zona (nombre_zona) VALUES  ('izquierda');
+INSERT INTO zona (nombre_zona) VALUES  ('bodega');
+INSERT INTO zona (nombre_zona) VALUES  ('cocina');
 
 INSERT INTO producto (codigo_producto, descripcion_producto, estado_producto, fecha_ingreso, img_producto, menu_cliente_producto, precio_venta_producto, stock_producto, id_marca, id_presentacion, id_zona) VALUES  ('001', 'papas con salsa', true, '2022-08-08', '', false, 2, 5, 1, 1, 1);
 
+INSERT INTO producto (codigo_producto, descripcion_producto, estado_producto, fecha_ingreso, img_producto, menu_cliente_producto, precio_venta_producto, stock_producto, id_marca, id_presentacion, id_zona) VALUES  ('002', 'Doritos dinamita', true, '2022-10-25', '', false, 5.6, 50, 2, 2, 2);
+
+INSERT INTO producto (codigo_producto, descripcion_producto, estado_producto, fecha_ingreso, img_producto, menu_cliente_producto, precio_venta_producto, stock_producto, id_marca, id_presentacion, id_zona) VALUES  ('003', 'Cola', true, '2022-10-23', '', false, 1.20, 10, 3, 3, 3);
+
 INSERT INTO detalle_proveedor (cuidad_detalle_proveedor, descripcion_detalle_proveedor, fecha_ingreso_detalle_proveedor) VALUES ('Cuenca', 'venta de snaks', '2022-08-03');
 
+INSERT INTO detalle_proveedor (cuidad_detalle_proveedor, descripcion_detalle_proveedor, fecha_ingreso_detalle_proveedor) VALUES ('Guayaquil', 'venta de Embutidos', '2022-10-21');
+
+INSERT INTO detalle_proveedor (cuidad_detalle_proveedor, descripcion_detalle_proveedor, fecha_ingreso_detalle_proveedor) VALUES ('Cuenca', 'venta de vegetales', '2022-09-15');
+
 INSERT INTO proveedor (direccion_proveedor, email_proveedor, estado_proveedor, movil_proveedor, nombre_proveedor, ruc_proveedor, telefono_proveedor, id_detalle_proveedor) VALUES ('checa', 'doritos@gmail.com', true, '0987535645', 'Doritos', '0105566046001', '', 1 );
+
+INSERT INTO proveedor (direccion_proveedor, email_proveedor, estado_proveedor, movil_proveedor, nombre_proveedor, ruc_proveedor, telefono_proveedor, id_detalle_proveedor) VALUES ('Lamar 5-56', 'piggiscuenca@gmail.com', true, '0967607728', 'Piggis', '0107081580001', '4259865', 2 );
+
+INSERT INTO proveedor (direccion_proveedor, email_proveedor, estado_proveedor, movil_proveedor, nombre_proveedor, ruc_proveedor, telefono_proveedor, id_detalle_proveedor) VALUES ('Ricaurte', 'megamaxi2022@gmail.com', true, '0993728565', 'Megamaxi', '0150399327001', '', 3 );
 
